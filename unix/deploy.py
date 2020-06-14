e19103fa0b8a481562dc44f3938e925eba30e3f8
Jang Rush <weakish@gmail.com>
2020-06-14 20:55:55 +0800

    :new: update alpine

--- unix/deploy.py
+++ unix/deploy.py
@@ -1,7 +1,7 @@
 from pyinfra.api import deploy
 from pyinfra.api.state import State
 from pyinfra.api.host import Host
-from pyinfra.modules import apt, python
+from pyinfra.modules import apk, apt, python
 from unix.operation import pkcon
 from unix.operation import freebsd
 
@@ -13,6 +13,9 @@ def update(state: State, host: Host) -> None:
         elif host.fact.linux_distribution["release_meta"]["ID"] == 'ubuntu':
             apt.update(state, host)
             apt.upgrade(state, host)
+        elif host.fact.linux_distribution["release_meta"]["ID"] == 'alpine':
+            apk.update(state, host)
+            apk.upgrade(state, host)
         else:
             python.raise_exception(state, host, NotImplementedError)
     elif host.fact.os == 'FreeBSD':
