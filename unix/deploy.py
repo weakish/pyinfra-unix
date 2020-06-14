9ff784989bd20df542ac72372ed4ee8c124cebbe
Jang Rush <weakish@gmail.com>
2020-06-14 20:49:41 +0800

    :new: update ubuntu

--- unix/deploy.py
+++ unix/deploy.py
@@ -1,7 +1,7 @@
 from pyinfra.api import deploy
 from pyinfra.api.state import State
 from pyinfra.api.host import Host
-from pyinfra.modules import python
+from pyinfra.modules import apt, python
 from unix.operation import pkcon
 from unix.operation import freebsd
 
@@ -10,6 +10,9 @@ def update(state: State, host: Host) -> None:
     if host.fact.os == 'Linux':
         if host.fact.linux_distribution["release_meta"]["ID"] == 'neon':
             pkcon.update(state, host)
+        elif host.fact.linux_distribution["release_meta"]["ID"] == 'ubuntu':
+            apt.update(state, host)
+            apt.upgrade(state, host)
         else:
             python.raise_exception(state, host, NotImplementedError)
     elif host.fact.os == 'FreeBSD':
